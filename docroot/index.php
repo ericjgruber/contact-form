e6b9c7394ea0caf02c17d2664ac1699391d0e1d4
Eric J Gruber <Eric.Gruber@vml.com>
2018-08-04 22:39:06 -0500

    Add radio inputs for form check

--- docroot/index.php
+++ docroot/index.php
@@ -9,27 +9,35 @@
 <body>
 
 <div class="container"> <!-- Start Bootstrap Container -->
-
-  <div class="row">
-    <h1>Contact Form</h1>
-    <p>Please fill out the following form below.</p>
-  </div>
+  <h1>Contact Form</h1>
+  <p>Please fill out the following form below.</p>
 
   <div class="row">
     <form action="" method="post" class="contact-form">
+      <div class="form-check">
+        <p>Is this position a full-time, direct-to-hire position?</p>
+        <input type="radio" class="form-check-input" name="radio-yes" id="radio-yes">
+        <label for="radio-yes" class="form-check-label">Yes</label>
+      </div>
+
+      <div class="form-check">
+        <input type="radio" class="form-check-input" name="radio-no" id="radio-no">
+        <label for="radio-no" class="form-check-label">No</label>
+      </div>
+
       <div class="form-group">
         <label for="name">Your name:</label>
-        <input type="text" name="name" id="name" class="form-control" required>
+        <input type="text" name="name" class="form-control" id="name" required>
       </div>
 
       <div class="form-group">
         <label for="email">Your email:</label>
-        <input type="email" name="email" id="email" required>
+        <input type="email" name="email" class="form-control" id="email" required>
       </div>
 
       <div class="form-group">
-        <label for="job-description">Please enter the job description here:</label>
-        <textarea name="job-description" id="job-description" rows="10" cols="60"></textarea>
+        <label for="job-description">Job description:</label>
+        <textarea name="job-description" class="form-control" id="job-description" rows="10"></textarea>
       </div>
 
       <button type="submit" class="btn btn-primary">Submit</button>
