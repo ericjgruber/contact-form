d24a7aab671b8659bedfc38823a21b3228bb9d2f
Eric J Gruber <Eric.Gruber@vml.com>
2018-08-04 22:12:19 -0500

    Add stylesheet and link to it

--- docroot/index.php
+++ docroot/index.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
   <head>
     <title>Contact Form</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
   </head>
 
 <body>
-
 <h1>Contact Form</h1>
 <p>Please fill out the following form below.</p>
 
 <form action="" method="post" class="contact-form">
   <div class="contact-form__inputs">
-    <label for="name">Enter your name:</label>
+    <label for="name">Your name:</label>
     <input type="text" name="name" id="name" required>
   </div>
 
   <div class="contact-form__inputs">
-    <label for="email">Enter your email:</label>
+    <label for="email">Your email:</label>
     <input type="email" name="email" id="email" required>
   </div>
 
