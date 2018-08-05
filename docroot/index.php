3a79fede3e72ad989bdf2054a2f91654b90f08a4
Eric J Gruber <Eric.Gruber@vml.com>
2018-08-04 22:26:20 -0500

    Add Bootstrap and references to it

--- docroot/index.php
+++ docroot/index.php
@@ -2,34 +2,43 @@
 <html lang="en-US">
   <head>
     <title>Contact Form</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
   </head>
 
 <body>
-<h1>Contact Form</h1>
-<p>Please fill out the following form below.</p>
 
-<form action="" method="post" class="contact-form">
-  <div class="contact-form__inputs">
-    <label for="name">Your name:</label>
-    <input type="text" name="name" id="name" required>
+<div class="container"> <!-- Start Bootstrap Container -->
+
+  <div class="row">
+    <h1>Contact Form</h1>
+    <p>Please fill out the following form below.</p>
   </div>
 
-  <div class="contact-form__inputs">
-    <label for="email">Your email:</label>
-    <input type="email" name="email" id="email" required>
+  <div class="row">
+    <form action="" method="post" class="contact-form">
+      <div class="form-group">
+        <label for="name">Your name:</label>
+        <input type="text" name="name" id="name" class="form-control" required>
+      </div>
+
+      <div class="form-group">
+        <label for="email">Your email:</label>
+        <input type="email" name="email" id="email" required>
+      </div>
+
+      <div class="form-group">
+        <label for="job-description">Please enter the job description here:</label>
+        <textarea name="job-description" id="job-description" rows="10" cols="60"></textarea>
+      </div>
+
+      <div class="contact-form__buttons">
+        <input type="submit" value="Submit">
+      </div>
+    </form>
   </div>
 
-  <div class="contact-form__inputs">
-    <label for="job-description">Please enter the job description here:</label>
-    <textarea name="job-description" id="job-description" rows="10" cols="60"></textarea>
-  </div>
-
-  <div class="contact-form__buttons">
-    <input type="submit" value="Submit">
-  </div>
-</form>
-
+</div> <!-- End Bootstrap Container -->
 </body>
 
 </html>
