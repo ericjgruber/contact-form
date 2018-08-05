2c8f85e479104fb04b198c8fece310cbe2fb68da
Eric J Gruber <Eric.Gruber@vml.com>
2018-08-04 21:59:26 -0500

    Initial commit

---
+++ docroot/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Contact Form</title>
+  </head>
+
+<body>
+
+<h1>Contact Form</h1>
+<p>Please fill out the following form below.</p>
+
+<form action="" method="post" class="contact-form">
+  <div class="contact-form__inputs"
+    <label for="name">Enter your name:</label>
+    <input type="text" name="name" id="name" required>
+  </div>
+
+  <div class="contact-form__inputs">
+    <label for="email">Enter your email:</label>
+    <input type="email" name="email" id="email" required>
+  </div>
+
+  <div class="contact-form__buttons">
+    <input type="submit" value="Submit">
+  </div>
+</form>
+
+</body>
+
+</html>
